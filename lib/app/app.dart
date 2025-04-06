74e4599402a41c5c7f8034102ce678b50dc39f74
Mostafa <mostafanaguib2015@gmail.com>
2025-04-06 19:11:50 +0200

    -- creating app themes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_project/presentation/utils/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -17,7 +18,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
   }
 }
 
